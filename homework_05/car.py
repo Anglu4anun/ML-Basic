9fca6f84e2f28c095b1bf8b377bdf18e40df6a42
Anglu4anun <93046979+Anglu4anun@users.noreply.github.com>
2025-11-24 13:31:43 +0300

    Update car.py

--- homework_05/car.py
+++ homework_05/car.py
@@ -1,3 +1,13 @@
 """
 Создайте класс `Car`, наследник `Vehicle`
 """
+
+"""решение"""
+from vehicle import Vehicle
+from engine import Engine
+class Car(Vehicle):
+    def __init__(self, weight: float, fuel: float, fuel_consumption: float):
+        super().__init__(weight, fuel, fuel_consumption)
+        self.engine = None
+    def set_engine(self, engine: Engine):
+        self.engine = engine
