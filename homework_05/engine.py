2cc8b89a2573dad5c473ed7aac8801fefc39293c
Anglu4anun <93046979+Anglu4anun@users.noreply.github.com>
2025-11-24 13:30:39 +0300

    Update engine.py

--- homework_05/engine.py
+++ homework_05/engine.py
@@ -1,3 +1,10 @@
 """
 Создайте dataclass `Engine`
 """
+
+"""решение"""
+from dataclasses import dataclass
+@dataclass
+class Engine:
+    volume: float           # Объем двигателя
+    pistons: int            # Количество цилиндров
